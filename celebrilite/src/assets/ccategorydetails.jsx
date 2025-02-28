f23e87b3c79da5ce81c21afe6cf8dc716fd5d962
MoneshCS <monesh.jangam@creditsafe.com>
2025-02-28 18:49:27 +0530

    Updated ccategorydetails.jsx

--- celebrilite/src/assets/ccategorydetails.jsx
+++ celebrilite/src/assets/ccategorydetails.jsx
@@ -132,7 +132,7 @@ const CCategoryDetails = () => {
             )}
 
             <button
-                onClick={() => navigate("/badminton")}
+                onClick={() => navigate("/carrom")}
                 className="mt-6 px-6 py-3 bg-blue-600 text-white font-bold rounded-lg hover:bg-blue-700 transition-all"
             >
                 Back to Categories
